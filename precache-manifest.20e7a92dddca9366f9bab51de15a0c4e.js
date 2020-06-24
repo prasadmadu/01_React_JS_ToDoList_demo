55afd8af2ce048f698d396baca8a4d505524e6cb
prasad <ncprasadmadusanka@gmail.com>
2020-06-25 02:53:28 +0530

    Updates

---
+++ precache-manifest.20e7a92dddca9366f9bab51de15a0c4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "691a57dba93935ab7808",
+    "url": "/01_React_JS_ToDoList_demo/static/css/main.a48cb97c.chunk.css"
+  },
+  {
+    "revision": "691a57dba93935ab7808",
+    "url": "/01_React_JS_ToDoList_demo/static/js/main.691a57db.chunk.js"
+  },
+  {
+    "revision": "04fd1540172756d7c168",
+    "url": "/01_React_JS_ToDoList_demo/static/js/1.04fd1540.chunk.js"
+  },
+  {
+    "revision": "ecf14f803ee895a44b23",
+    "url": "/01_React_JS_ToDoList_demo/static/js/runtime~main.ecf14f80.js"
+  },
+  {
+    "revision": "4b3332d755dca3e3b21fa93bd4a57125",
+    "url": "/01_React_JS_ToDoList_demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/01_React_JS_ToDoList_Demo/precache-manifest.c1d4bdca4ba1000966bf72ff1c646358.js"
+  "/01_React_JS_ToDoList_demo/precache-manifest.20e7a92dddca9366f9bab51de15a0c4e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/01_React_JS_ToDoList_Demo/index.html", {
+workbox.routing.registerNavigationRoute("/01_React_JS_ToDoList_demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
