27a5ac9ef09dfd103f376ca51e6257e558e6c6e9
prasad <ncprasadmadusanka@gmail.com>
2020-06-25 02:22:58 +0530

    Updates

---
+++ precache-manifest.c1d4bdca4ba1000966bf72ff1c646358.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33dce6ac6b5e4e76942e",
+    "url": "/01_React_JS_ToDoList_Demo/static/css/main.a48cb97c.chunk.css"
+  },
+  {
+    "revision": "33dce6ac6b5e4e76942e",
+    "url": "/01_React_JS_ToDoList_Demo/static/js/main.33dce6ac.chunk.js"
+  },
+  {
+    "revision": "04fd1540172756d7c168",
+    "url": "/01_React_JS_ToDoList_Demo/static/js/1.04fd1540.chunk.js"
+  },
+  {
+    "revision": "dc9f61bcd05fd1c185f5",
+    "url": "/01_React_JS_ToDoList_Demo/static/js/runtime~main.dc9f61bc.js"
+  },
+  {
+    "revision": "0690ca6ca5a2e5ad0bef91d91c47fc8f",
+    "url": "/01_React_JS_ToDoList_Demo/index.html"
+  }
+];
